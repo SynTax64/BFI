4245134352fcabfde1581c1d68d89c5e8bc9ce3a
David A <davidaster007@gmail.com>
2021-03-09 17:22:30 +0100

    added some searching methods

--- src/at/bfi/basics/teil1_aufgaben/searching.java
+++ src/at/bfi/basics/teil1_aufgaben/searching.java
@@ -1,5 +1,6 @@
 package at.bfi.basics.teil1_aufgaben;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -32,21 +33,48 @@ public class searching {
 		return -1;
 	}
 
+	public static int[] tauscheElemente(int[] array) {
+//		int[] neuArray = new int[array.length];
+		for (int i = 0; i < array.length / 2; i++) {
+			int tmp = array[array.length - 1 - i];
+			array[array.length - 1 - i] = array[i];
+			array[i] = tmp;
+		}
+
+		return array;
+	}
+
+	public static int[] cloneMe(int[] array) {
+		int[] cloneArray = new int[array.length];
+		for (int i = 0; i < array.length; i++) {
+			cloneArray[i] = array[i];
+		}
+		return cloneArray;
+	}
+
 	public static void main(String[] args) {
 		Random rnd = new Random();
 		Scanner input = new Scanner(System.in);
 
-		int[] intArray = new int[rnd.nextInt(100)];
-
+		int[] intArray = new int[10];
+//		int[] cloneArray = intArray.clone();
 		for (int i = 0; i < intArray.length; i++) {
-			intArray[i] = rnd.nextInt(1000);
+			intArray[i] = rnd.nextInt(10);
 		}
-
-		System.out.println("Bitte geben sie eine Zahl ein: ");
-		int zahl = Integer.parseInt(input.nextLine());
 
 		for (int i = 0; i < intArray.length; i++) {
 			System.out.print(intArray[i] + " ");
 		}
+		System.out.println();
+		System.out.println(Arrays.toString(intArray));
+		tauscheElemente(intArray);
+
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
+		}
+
+		System.out.println();
+
+		System.out.println(Arrays.toString(intArray));
 	}
 }
