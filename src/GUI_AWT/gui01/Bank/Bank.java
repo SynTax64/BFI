91ec25b8e7e14f60134c81daf3686e41f333d9f1
David Aster <davidaster007@gmail.com>
2021-04-13 07:43:38 +0200

    deleted unnecessary packages

--- src/GUI_AWT/gui01/Bank/Bank.java
+++
@@ -1,9 +0,0 @@
-package GUI_AWT.gui01.Bank;
-
-public class Bank {
-
-	public static void main(String[] args) {
-		BankView bankView = new BankView();
-
-	}
-}
--- src/GUI_SWING/gui01/Bank.java
+++
@@ -1,9 +0,0 @@
-package GUI_SWING.gui01;
-
-public class Bank {
-
-	public static void main(String[] args) {
-		BankView bankView = new BankView();
-
-	}
-}
